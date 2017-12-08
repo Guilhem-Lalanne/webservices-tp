10e3cb663454cf50fa2a9f8c77826a1da345219e
Guilhem-Lalanne <guilhem@asus>
2017-12-08 21:59:32 +0100

    BugFix : target address + return value

--- frontend/controller.php
+++ frontend/controller.php
@@ -6,13 +6,15 @@
  * Time: 16:40
  */
 
-var $target = "http://webservices-tp.localhost/web/app.php";
+$target = "http://webservices-tp.localhost/web/app_dev.php";
 
 if (isset($_POST) and !empty($_POST)) {
     $initialCurrencyCode = $_POST['initialCurrencyCode'];
     $targetCurrencyCode = $_POST['targetCurrencyCode'];
     $amount = $_POST['amount'];
-    $target .= $initialCurrencyCode . "/" . $targetCurrencyCode . "/" . $amount;
+    $target .= "/" . $initialCurrencyCode . "/" . $targetCurrencyCode . "/" . $amount;
 
     $value = file_get_contents($target);
+
+    print_r($value);
 }
