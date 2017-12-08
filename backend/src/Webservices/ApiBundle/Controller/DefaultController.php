1aac9b9ab6705da5ac56648587afe47337027c6d
Guilhem-Lalanne <guilhem@asus>
2017-12-08 15:44:39 +0100

    Add function convertAction in controller.
    It returns the result of the convertion passed in the address bar.

--- backend/src/Webservices/ApiBundle/Controller/DefaultController.php
+++ backend/src/Webservices/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,38 @@
 namespace Webservices\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Webservices\ApiBundle\Entity\Currency;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('WebservicesApiBundle:Default:index.html.twig');
+    }
+
+    public function convertAction($initialCurrencyCode, $targetCurrencyCode, $amount)
+    {
+
+        $em = $this->getDoctrine();
+
+        $convertedAmount = $em
+            ->getRepository(Currency::class)
+            ->getConvertedAmount($initialCurrencyCode, $targetCurrencyCode, $amount);
+
+//        // Test json :
+//        $response = new JsonResponse();
+//        $response->setData(json_encode($convertedAmount));
+//        $response->headers->set('Content-Type', 'application/json');
+
+        $response = new Response(
+            $convertedAmount,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+        );
+
+        return $response;
     }
 }
